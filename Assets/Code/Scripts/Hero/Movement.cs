1e5c0964f34f6efc10690a4be3e74859ffc12b7e
yuripavlowww <yuripavlowww@gmail.com>
2023-11-18 22:39:45 +0300

    feat: add weapon models, hero's movement

---
+++ Assets/Code/Scripts/Hero/Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float speed = 6.0f;
+    private CharacterController characterController;
+    private float gravity = -9.8f; 
+
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed) * Time.deltaTime;
+        movement.y = gravity;
+        movement = transform.TransformDirection(movement);
+
+        characterController.Move(movement);
+    }
+}
